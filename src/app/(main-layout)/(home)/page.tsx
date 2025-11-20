08e97a3b42532c7ab0e1e9fa940d8eac1a56d5c2
Yead191 <yead191@gmail.com>
2025-11-20 16:31:30 +0600

    simple modification

---
+++ src/app/(main-layout)/(home)/page.tsx
@@ -0,0 +1,11 @@
+
+export default function Home() {
+  return (
+    <section >
+      <h1 className="text-primary text-6xl min-h-screen flex justify-center items-center">
+
+      welcome to yead template!
+      </h1>
+    </section>
+  );
+}
